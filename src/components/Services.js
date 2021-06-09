bcdd8ce5a6ad81452842d30be25ad0082ac4b819
Austin Schrader <austin.jschrader@gmail.com>
2021-06-08 20:53:53 -0700

    Add basic services component

---
+++ src/components/Services.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({}));
+
+export default function Services(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <div style={{ height: '2600px' }}>
+      <h1>This is our past sales!!</h1>
+    </div>
+  );
+}
